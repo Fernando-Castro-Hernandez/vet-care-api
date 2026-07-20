b4ba9bce705d7400e6f16c3c7ca96aeb3e882643
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2026-07-20 11:45:58 -0600

    first commit

---
+++ src/main/java/mx/edu/tecdesoftware/vet_care_api/VetCareApiApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.vet_care_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/tecdesoftware/vet_care_api/VetCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.vet_care_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
